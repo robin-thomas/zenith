2910a070a8b6fcd8b9906d6136396efe711d5343
Robin Thomas <robinthomas2591@gmail.com>
2023-05-17 16:56:12 +0800

    read from smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,15 @@
-import { config as dotenvConfig } from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from 'dotenv';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 dotenvConfig();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
-  defaultNetwork: "polygon_mumbai",
+  solidity: '0.8.18',
+  defaultNetwork: 'polygon_mumbai',
   networks: {
     polygon_mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
+      url: 'https://rpc-mumbai.maticvigil.com',
       accounts: [process.env.PRIVATE_KEY as string],
     },
   },
---
+++ src/app/api/stats/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+import { providers, Contract } from 'ethers';
+
+import Zenith from '../../../../artifacts/contracts/Zenith.sol/Zenith.json';
+
+export async function GET() {
+  const provider = new providers.AlchemyProvider('maticmum', process.env.ALCHEMY_KEY as string);
+  const contract = new Contract(process.env.NEXT_PUBLIC_CONTRACT_ADDRESS as string, Zenith.abi, provider);
+
+  const campaigns = await contract.numCampaigns();
+  const adClicks = await contract.numAdClicks();
+
+  return NextResponse.json({
+    campaigns: campaigns.toNumber(),
+    adClicks: adClicks.toNumber(),
+  });
+}
