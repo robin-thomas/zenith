c0feddc3c416bf7508465a131c266d6e7384038a
Robin Thomas <robinthomas2591@gmail.com>
2023-05-19 09:52:15 +0800

    fix build

--- src/layouts/data-grid/CampaignGrid.types.ts
+++ src/layouts/data-grid/CampaignGrid.types.ts
@@ -3,13 +3,12 @@ import type { Dayjs } from 'dayjs';
 export interface CampaignGridData {
   status: string;
   name: string;
-  description: string;
   budget: number;
   url: string;
   remaining: number;
   clicks: number;
-  startDate: Dayjs;
-  endDate: Dayjs;
+  created: Dayjs;
+  end: Dayjs;
 }
 
 export interface CampaignGridProps {
