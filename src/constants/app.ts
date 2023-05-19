8adbc43eb287ef503379828a8dd5186c4a40301c
Robin Thomas <robinthomas2591@gmail.com>
2023-05-19 12:31:53 +0800

    improve home page

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = 'Zenith';
+export const APP_DESCRIPTION_SHORT = 'A decentralized advertising platform.';
+export const APP_DESCRIPTION_EXTRA = 'Powered by Ethereum, Chainlink';
 export const CURRENCY_SYMBOL = 'Ξ';
 export const CURRENCY_NAME = 'Ether';
