920b91ef2f85f5ea3bff51991810edf0f330c246
Robin Thomas <robinthomas2591@gmail.com>
2023-05-27 18:38:21 +0800

    ui improvs

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = 'Zenith';
 export const APP_NAME_CAPS = APP_NAME.toUpperCase();
 export const APP_DESCRIPTION_SHORT = 'A decentralized advertising platform.';
-export const APP_DESCRIPTION_EXTRA = 'Powered by Polygon, Chainlink, SxT';
+export const APP_DESCRIPTION_EXTRA = 'Powered by Polygon, Chainlink, SxT, Truflation';
 export const CURRENCY_SYMBOL = '∾';
 export const CURRENCY_NAME = 'MATIC';
