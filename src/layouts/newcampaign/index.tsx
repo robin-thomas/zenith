f7149b754e595ecbb65243ec700034f07889ffc4
Robin Thomas <robinthomas2591@gmail.com>
2023-05-28 13:27:14 +0800

    use gitcoin passport in creating campaign

--- src/layouts/newcampaign/index.tsx
+++ src/layouts/newcampaign/index.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+import Tooltip from '@mui/material/Tooltip';
 import Grid from '@mui/material/Unstable_Grid2';
 import InputAdornment from '@mui/material/InputAdornment';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import StepContent from '@mui/material/StepContent';
+import LoadingButton from '@mui/lab/LoadingButton';
 import { Formik } from 'formik';
 import { object as YupObject, string as YupString, number as YupNumber, date as YupDate } from 'yup';
 import dayjs from 'dayjs';
+import Alert from '@mui/material/Alert';
 
 import Payment from '@/layouts/newcampaign/payment/Payment';
 import CampaignEnd from '@/layouts/newcampaign/CampaignEnd';
@@ -17,6 +20,8 @@ import Preview from '@/layouts/newcampaign/Preview';
 import TextInput from '@/layouts/newcampaign/textinput/TextInput';
 import { PLACEHOLDER_NAME, PLACEHOLDER_DESCRIPTION, PLACEHOLDER_URL } from '@/constants/campaign';
 import { CURRENCY_SYMBOL, CURRENCY_NAME } from '@/constants/app';
+import { useAppContext } from '@/hooks/useAppContext';
+import Link from 'next/link';
 
 const validationSchema = YupObject({
   name: YupString(),
@@ -28,7 +33,46 @@ const validationSchema = YupObject({
 });
 
 const NewCampaign: React.FC = () => {
+  const [score, setScore] = useState<number>();
   const [activeStep, setActiveStep] = useState(0);
+  const [submitting, setSubmitting] = useState(false);
+
+  const { wallet } = useAppContext();
+
+  useEffect(() => {
+    const fn = async () => {
+      const resp = await fetch(`/api/passport/score/${wallet.accounts[0]}`);
+      const data = await resp.json();
+
+      setScore(data?.score ?? null);
+    };
+
+    if (wallet && score === undefined) {
+      fn();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [wallet, score]);
+
+  const onSubmitPassport = async () => {
+    setSubmitting(true);
+
+    const passportResp = await fetch('/api/passport', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        address: wallet.accounts[0],
+      }),
+    });
+
+    await passportResp.json();
+
+    setTimeout(() => {
+      setSubmitting(false);
+      setScore(undefined);
+    }, 1000);
+  };
 
   return (
     <Formik
@@ -50,9 +94,28 @@ const NewCampaign: React.FC = () => {
           <Step>
             <StepLabel>Details</StepLabel>
             <StepContent>
-              <>
-                Fill in the details about your campaign.
-              </>
+              <Tooltip title="Higher your trust score, better your chances at winning the bidding auction" arrow>
+                <Alert
+                  severity="info"
+                  variant="outlined"
+                  action={(
+                    <LoadingButton
+                      size="small"
+                      variant="contained"
+                      loading={submitting}
+                      onClick={onSubmitPassport}
+                    >
+                      Recheck Score
+                    </LoadingButton>
+                  )}
+                >
+                  Your <i>Gitcoin Passport</i> Score: <b>{score}</b>.
+                  Increase your score by adding more stamps at:&nbsp;
+                  <Link href="https://passport.gitcoin.co/#/dashboard" target="_blank">
+                    Gitcoin Passport
+                  </Link>
+                </Alert>
+              </Tooltip>
               <TextInput id="name" label="Campaign Name" placeholder={PLACEHOLDER_NAME} />
               <TextInput id="description" rows={2} label="Campaign Description" placeholder={PLACEHOLDER_DESCRIPTION} />
               <Grid container spacing={2}>
