735c9b19c36529248523a85d254d6b60c2643795
Robin Thomas <robinthomas2591@gmail.com>
2023-06-01 16:34:28 +0800

    fix the home link in logged in mode

--- src/layouts/typography/logo/Logo.tsx
+++ src/layouts/typography/logo/Logo.tsx
@@ -4,16 +4,27 @@ import { Montserrat_Alternates } from 'next/font/google';
 
 import styles from './Logo.module.css';
 import { APP_NAME } from '@/constants/app';
+import type { LogoProps } from './Logo.types';
 
 const montserrat_alternates = Montserrat_Alternates({ weight: '700', subsets: ['latin'] });
 
-const Logo = () => (
-  <Link href="/">
-    <div className={styles.appname}>
-      <Image src="/logo.png" width="40" height="17" alt="logo" />&nbsp;
-      <span className={montserrat_alternates.className}>{APP_NAME}.</span>
-    </div>
-  </Link>
+const LogoComponent = () => (
+  <div className={styles.appname}>
+    <Image src="/logo.png" width="40" height="17" alt="logo" />&nbsp;
+    <span className={montserrat_alternates.className}>{APP_NAME}.</span>
+  </div>
 );
+
+const Logo: React.FC<LogoProps> = ({ disableLink }) => {
+  if (disableLink) {
+    return <LogoComponent />;
+  }
+
+  return (
+    <Link href="/">
+      <LogoComponent />
+    </Link>
+  );
+};
 
 export default Logo;
