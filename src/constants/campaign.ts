eb19a5ba1c32ba3152225224b0c9f0b63bb7f199
Robin Thomas <robinthomas2591@gmail.com>
2023-06-08 12:10:52 +0800

    update default campaign description

--- src/constants/campaign.ts
+++ src/constants/campaign.ts
@@ -1,3 +1,3 @@
 export const PLACEHOLDER_NAME = 'Zenith: The Decentralised Advertising Platform';
-export const PLACEHOLDER_DESCRIPTION = 'Unlock advertising\'s future: Transparency, efficiency, and fairness. Verifiable transactions for advertisers, and fair rewards for publishers. Experience it now!';
+export const PLACEHOLDER_DESCRIPTION = 'Unlock future of advertising: Transparency, efficiency, and fairness. Verifiable transactions for advertisers, and fair rewards for publishers. Experience it now!';
 export const PLACEHOLDER_URL = 'https://zenith-six.vercel.app';
